153fcab3b008a9700c9b6cc6e752812b9f8e4f0d
Andrew Gordienko <50226581+ittrainbow@users.noreply.github.com>
2023-07-29 00:31:09 +0300

    origin headers 2

--- index.js
+++ index.js
@@ -14,12 +14,6 @@ const io = new Server(server, {
 app.use(cors())
 app.use(router)
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-
 const PORT = 5001
 server.listen(PORT, () => console.log(`Server is running at ${PORT}`))
 
