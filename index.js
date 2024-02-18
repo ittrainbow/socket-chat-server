e5c66a0d44faba9c8150eb1cbf2ef93c72117c3a
Andrew Gordienko <50226581+ittrainbow@users.noreply.github.com>
2024-02-19 01:44:48 +0300

    cors 2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const http = require('http')
 const { Server } = require('socket.io')
-const cors = require('cors')
+// const cors = require('cors')
 const { findUser, findRoom, compareUsers, getRoomUsers, broadcastRooms } = require('./helpers')
 
 const app = express()
 const server = http.createServer(app)
 const router = express.Router()
 const io = new Server(server, {
-  cors: { origin: '*', methods: ['GET', 'POST'], allowedHeaders: ['Access-Control-Allow-Origin'] }
+  // cors: { origin: '*', methods: ['GET', 'POST'], allowedHeaders: ['Access-Control-Allow-Origin'] }
 })
 
 // app.use(cors())
